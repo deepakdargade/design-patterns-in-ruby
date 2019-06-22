141acb044edc12df534bea9162d6330925d3da05
Deepak Dargade <deepakdargade@gmail.com>
2019-06-22 16:02:35 +0530

    Stategy pattern added

---
+++ strategy_pattern_2.rb
@@ -0,0 +1,51 @@
+class Formatter
+  def output_report(title, text)
+    raise 'abstract method called - output_report'
+  end
+end
+
+class HTMLFormatter < Formatter
+  def output_report(title, text)
+    puts '<html>'
+    puts '<head>'
+    puts "<title>#{title}</title>"
+    puts '</head>'
+    puts '<body>'
+    text.each do |line|
+      puts "<p>#{line}</p>"
+    end
+    puts '</body>'
+    puts '</html>'
+  end
+end
+
+class PlainTextFormatter < Formatter
+  def output_report(title, text)
+    puts "***#{title}***"
+    text.each do |line|
+      puts line
+    end
+  end
+end
+
+class Report
+  attr_reader :title, :text
+  attr_accessor :formatter
+
+  def initialize(formatter)
+    @title = 'Monthly Report'
+    @text = ['Today is', 'Saturday !!!']
+    @formatter = formatter
+  end
+
+  def output_report
+    @formatter.output_report(@title, @text)
+  end
+end
+
+
+report = Report.new(HTMLFormatter.new)
+report.output_report
+
+report = Report.new(PlainTextFormatter.new)
+report.output_report
